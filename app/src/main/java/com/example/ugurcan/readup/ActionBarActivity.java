fc80ef99df5c1e79452f2a7b86515a856ee6f7d1
Ugurcan <ugurcan.eken@gmail.com>
2016-11-29 19:34:44 +0300

    ActionBarActivity tekrar  düzenlendi.

---
+++ app/src/main/java/com/example/ugurcan/readup/ActionBarActivity.java
@@ -0,0 +1,48 @@
+package com.example.ugurcan.readup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by Ugurcan on 28.11.2016.
+ */
+
+public class ActionBarActivity  extends AppCompatActivity {
+    EditText searchWord;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+
+             Toast.makeText(getApplicationContext(),searchWord.getText().toString(),Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if(id==R.id.action_addFile)
+        {
+            Toast.makeText(getApplicationContext(),searchWord.getText().toString(),Toast.LENGTH_LONG).show();
+            return  true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
