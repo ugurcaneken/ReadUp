7eb5dae32906db4a557efd1249b9070e355fe726
dupebi <duygupelinbilgin@hotmail.com>
2016-11-29 21:38:19 +0300

    ActionbarActivitye eklemeler yapıldı

--- app/src/main/java/com/example/ugurcan/readup/ActionBarActivity.java
+++ app/src/main/java/com/example/ugurcan/readup/ActionBarActivity.java
@@ -1,6 +1,7 @@
 package com.example.ugurcan.readup;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -9,12 +10,15 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.folderselector.FileChooserDialog;
+
+import java.io.File;
 
 /**
  * Created by Ugurcan on 28.11.2016.
  */
 
-public class ActionBarActivity  extends AppCompatActivity {
+public class ActionBarActivity  extends AppCompatActivity  implements FileChooserDialog.FileCallback {
     EditText searchWord;
 
     @Override
@@ -37,20 +41,30 @@ public class ActionBarActivity  extends AppCompatActivity {
         if (id == R.id.action_search) {
 
             new MaterialDialog.Builder(this)
-                    .title("Merhaba")
-                    .content("ReadUp Projesi")
+                    .title("Arama Sonuçları")
+                    .content("Love")
                     .positiveText("OK")
-                    .negativeText("İPTAL")
-                    .neutralText("devamı...")
                     .show();
             return true;
         }
         if(id==R.id.action_addFile)
         {
-            Toast.makeText(getApplicationContext(),searchWord.getText().toString(),Toast.LENGTH_LONG).show();
+            new FileChooserDialog.Builder(this)
+                    .initialPath("/sdcard/Download")  // changes initial path, defaults to external storage directory
+                    .mimeType("image/*") // Optional MIME type filter
+                    .extensionsFilter(".pdf") // Optional extension filter, will override mimeType()
+                    .tag("optional-identifier")
+                    .goUpLabel("Up") // custom go up label, default label is "..."
+                    .show();
             return  true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFileSelection(@NonNull FileChooserDialog dialog, @NonNull File file) {
+        final String tag = dialog.getTag();
+
     }
 }
