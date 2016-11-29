01eab44d4c34508014556a5a43be4e99e058a795
Ugurcan <ugurcan.eken@gmail.com>
2016-11-29 19:22:00 +0300

    ActionBarActivity tekrar  düzenlendi.

--- app/src/main/java/com/example/ugurcan/readup/MainActivity.java
+++ app/src/main/java/com/example/ugurcan/readup/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.ugurcan.readup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
